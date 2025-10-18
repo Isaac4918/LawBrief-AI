dce0c5508f5ae0ad8d4f4a856731485faafad588
Isaac Araya Solano <38987656+Isaac4918@users.noreply.github.com>
2025-10-17 20:36:53 -0600

    initial files to create the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <div className="chat-container">
+        <ChatBox />
+      </div>
+    </div>
+  );
+}
+
+export default App;
